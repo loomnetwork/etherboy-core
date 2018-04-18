0d4009d384d23a6b5d241f850d9cac2c9b1f0993
Aditya Godbole <aditya@oogway.in>
2018-04-18 15:32:30 +0530

    initial etherboy plugin

---
+++ etherboy.go
@@ -0,0 +1,97 @@
+// go build -buildmode=plugin -o contracts/helloworld.so plugin/examples/helloworld.go
+package main
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/loomnetwork/etherboy-core/gen"
+	"github.com/loomnetwork/loom/plugin"
+	proto "github.com/golang/protobuf/proto"
+	"strings"
+	"github.com/loomnetwork/loom"
+)
+
+func main() {
+}
+
+type EtherBoy struct {
+}
+
+func (e *EtherBoy) Meta() plugin.Meta {
+	return plugin.Meta{
+		Name:    "etherboycore",
+		Version: "0.0.1",
+	}
+}
+
+func (e *EtherBoy) Init(ctx plugin.Context, req *plugin.Request) (*plugin.Response, error) {
+	println("init contract")
+	return &plugin.Response{}, nil
+}
+
+func (e *EtherBoy) Call(ctx plugin.Context, req *plugin.Request) (*plugin.Response, error) {
+	var tx txmsg.EtherboyAppTx
+	proto.Unmarshal(req.Body, &tx)
+	owner := strings.TrimSpace(tx.Owner)
+	switch tx.Type {
+	case "CreateAccount":
+		createAccTx := tx.GetCreateAccount()
+		if err := e.createAccount(ctx, owner, createAccTx); err != nil {
+			return e.jsonResponse(), err
+		}
+	case "SaveState":
+		saveStateTx := tx.GetState()
+		if err:= e.saveState(ctx, owner, saveStateTx); err != nil {
+			return e.jsonResponse(), err
+		}
+	}
+	return &plugin.Response{}, nil
+}
+
+func (e *EtherBoy) jsonResponse() *plugin.Response {
+	return &plugin.Response{
+		ContentType: plugin.ContentType_JSON,
+		Body: []byte("{}"),
+	}
+}
+
+func (e *EtherBoy) StaticCall(ctx plugin.StaticContext, req *plugin.Request) (*plugin.Response, error) {
+	return &plugin.Response{}, nil
+}
+
+func (e *EtherBoy) ownerKey(owner string) []byte {
+	return []byte("owner:" + owner)
+}
+
+func (e *EtherBoy) createAccount(ctx plugin.Context, owner string, accTx *txmsg.EtherboyCreateAccountTx) error {
+	// confirm owner doesnt exist already
+	if ctx.Has(e.ownerKey(owner)) {
+		return errors.New("Owner already exists")
+	}
+	state := &txmsg.EtherboyAppState {
+		Address: []byte(ctx.Message().Sender.Local),
+	}
+	statebytes, err := proto.Marshal(state)
+	if err != nil {
+		return errors.Wrap(err, "Error marshaling state node")
+	}
+	ctx.Set(e.ownerKey(owner), statebytes)
+	return nil
+}
+
+func (e *EtherBoy) saveState(ctx plugin.Context, owner string, tx *txmsg.EtherboyStateTx) error {
+	var curState txmsg.EtherboyAppState
+	proto.Unmarshal(ctx.Get(e.ownerKey(owner)), &curState)
+	if loom.LocalAddress(curState.Address).Compare(ctx.Message().Sender.Local) != 0 {
+		return errors.New("Owner unverified")
+	}
+	curState.Blob = tx.Data
+	statebytes, err := proto.Marshal(&curState)
+	if err != nil {
+		return errors.Wrap(err, "Error marshaling state node")
+	}
+	ctx.Set(e.ownerKey(owner), statebytes)
+	return nil
+}
+
+var Contract plugin.Contract = &EtherBoy{}
--- gen/txmsg.pb.go
+++ gen/txmsg.pb.go
@@ -8,9 +8,10 @@ It is generated from these files:
 	txmsg.proto
 
 It has these top-level messages:
-	EtherboyCreateAccount
-	EtherboyState
+	EtherboyCreateAccountTx
+	EtherboyStateTx
 	EtherboyAppTx
+	EtherboyAppState
 */
 package txmsg
 
@@ -29,33 +30,33 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type EtherboyCreateAccount struct {
+type EtherboyCreateAccountTx struct {
 	// public is a json object that the dApp can look into
 	Data []byte `protobuf:"bytes,1,opt,name=data,proto3" json:"data,omitempty"`
 }
 
-func (m *EtherboyCreateAccount) Reset()                    { *m = EtherboyCreateAccount{} }
-func (m *EtherboyCreateAccount) String() string            { return proto.CompactTextString(m) }
-func (*EtherboyCreateAccount) ProtoMessage()               {}
-func (*EtherboyCreateAccount) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+func (m *EtherboyCreateAccountTx) Reset()                    { *m = EtherboyCreateAccountTx{} }
+func (m *EtherboyCreateAccountTx) String() string            { return proto.CompactTextString(m) }
+func (*EtherboyCreateAccountTx) ProtoMessage()               {}
+func (*EtherboyCreateAccountTx) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
-func (m *EtherboyCreateAccount) GetData() []byte {
+func (m *EtherboyCreateAccountTx) GetData() []byte {
 	if m != nil {
 		return m.Data
 	}
 	return nil
 }
 
-type EtherboyState struct {
+type EtherboyStateTx struct {
 	Data []byte `protobuf:"bytes,1,opt,name=data,proto3" json:"data,omitempty"`
 }
 
-func (m *EtherboyState) Reset()                    { *m = EtherboyState{} }
-func (m *EtherboyState) String() string            { return proto.CompactTextString(m) }
-func (*EtherboyState) ProtoMessage()               {}
-func (*EtherboyState) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (m *EtherboyStateTx) Reset()                    { *m = EtherboyStateTx{} }
+func (m *EtherboyStateTx) String() string            { return proto.CompactTextString(m) }
+func (*EtherboyStateTx) ProtoMessage()               {}
+func (*EtherboyStateTx) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
-func (m *EtherboyState) GetData() []byte {
+func (m *EtherboyStateTx) GetData() []byte {
 	if m != nil {
 		return m.Data
 	}
@@ -66,7 +67,8 @@ type EtherboyAppTx struct {
 	// version to help upgrades to code
 	Version int32 `protobuf:"varint,1,opt,name=version" json:"version,omitempty"`
 	// dispatch based on different transaction types
-	Type string `protobuf:"bytes,2,opt,name=type" json:"type,omitempty"`
+	Type  string `protobuf:"bytes,2,opt,name=type" json:"type,omitempty"`
+	Owner string `protobuf:"bytes,3,opt,name=owner" json:"owner,omitempty"`
 	// the actual message body
 	//
 	// Types that are valid to be assigned to Data:
@@ -85,10 +87,10 @@ type isEtherboyAppTx_Data interface {
 }
 
 type EtherboyAppTx_CreateAccount struct {
-	CreateAccount *EtherboyCreateAccount `protobuf:"bytes,16,opt,name=create_account,json=createAccount,oneof"`
+	CreateAccount *EtherboyCreateAccountTx `protobuf:"bytes,16,opt,name=create_account,json=createAccount,oneof"`
 }
 type EtherboyAppTx_State struct {
-	State *EtherboyState `protobuf:"bytes,3,opt,name=state,oneof"`
+	State *EtherboyStateTx `protobuf:"bytes,8,opt,name=state,oneof"`
 }
 
 func (*EtherboyAppTx_CreateAccount) isEtherboyAppTx_Data() {}
@@ -115,14 +117,21 @@ func (m *EtherboyAppTx) GetType() string {
 	return ""
 }
 
-func (m *EtherboyAppTx) GetCreateAccount() *EtherboyCreateAccount {
+func (m *EtherboyAppTx) GetOwner() string {
+	if m != nil {
+		return m.Owner
+	}
+	return ""
+}
+
+func (m *EtherboyAppTx) GetCreateAccount() *EtherboyCreateAccountTx {
 	if x, ok := m.GetData().(*EtherboyAppTx_CreateAccount); ok {
 		return x.CreateAccount
 	}
 	return nil
 }
 
-func (m *EtherboyAppTx) GetState() *EtherboyState {
+func (m *EtherboyAppTx) GetState() *EtherboyStateTx {
 	if x, ok := m.GetData().(*EtherboyAppTx_State); ok {
 		return x.State
 	}
@@ -147,7 +156,7 @@ func _EtherboyAppTx_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
 			return err
 		}
 	case *EtherboyAppTx_State:
-		b.EncodeVarint(3<<3 | proto.WireBytes)
+		b.EncodeVarint(8<<3 | proto.WireBytes)
 		if err := b.EncodeMessage(x.State); err != nil {
 			return err
 		}
@@ -165,15 +174,15 @@ func _EtherboyAppTx_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.
 		if wire != proto.WireBytes {
 			return true, proto.ErrInternalBadWireType
 		}
-		msg := new(EtherboyCreateAccount)
+		msg := new(EtherboyCreateAccountTx)
 		err := b.DecodeMessage(msg)
 		m.Data = &EtherboyAppTx_CreateAccount{msg}
 		return true, err
-	case 3: // data.state
+	case 8: // data.state
 		if wire != proto.WireBytes {
 			return true, proto.ErrInternalBadWireType
 		}
-		msg := new(EtherboyState)
+		msg := new(EtherboyStateTx)
 		err := b.DecodeMessage(msg)
 		m.Data = &EtherboyAppTx_State{msg}
 		return true, err
@@ -193,7 +202,7 @@ func _EtherboyAppTx_OneofSizer(msg proto.Message) (n int) {
 		n += s
 	case *EtherboyAppTx_State:
 		s := proto.Size(x.State)
-		n += proto.SizeVarint(3<<3 | proto.WireBytes)
+		n += proto.SizeVarint(8<<3 | proto.WireBytes)
 		n += proto.SizeVarint(uint64(s))
 		n += s
 	case nil:
@@ -203,26 +212,54 @@ func _EtherboyAppTx_OneofSizer(msg proto.Message) (n int) {
 	return n
 }
 
+type EtherboyAppState struct {
+	Address []byte `protobuf:"bytes,1,opt,name=address,proto3" json:"address,omitempty"`
+	Blob    []byte `protobuf:"bytes,2,opt,name=blob,proto3" json:"blob,omitempty"`
+}
+
+func (m *EtherboyAppState) Reset()                    { *m = EtherboyAppState{} }
+func (m *EtherboyAppState) String() string            { return proto.CompactTextString(m) }
+func (*EtherboyAppState) ProtoMessage()               {}
+func (*EtherboyAppState) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
+
+func (m *EtherboyAppState) GetAddress() []byte {
+	if m != nil {
+		return m.Address
+	}
+	return nil
+}
+
+func (m *EtherboyAppState) GetBlob() []byte {
+	if m != nil {
+		return m.Blob
+	}
+	return nil
+}
+
 func init() {
-	proto.RegisterType((*EtherboyCreateAccount)(nil), "EtherboyCreateAccount")
-	proto.RegisterType((*EtherboyState)(nil), "EtherboyState")
+	proto.RegisterType((*EtherboyCreateAccountTx)(nil), "EtherboyCreateAccountTx")
+	proto.RegisterType((*EtherboyStateTx)(nil), "EtherboyStateTx")
 	proto.RegisterType((*EtherboyAppTx)(nil), "EtherboyAppTx")
+	proto.RegisterType((*EtherboyAppState)(nil), "EtherboyAppState")
 }
 
 func init() { proto.RegisterFile("txmsg.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 192 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2e, 0xa9, 0xc8, 0x2d,
-	0x4e, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x57, 0xd2, 0xe6, 0x12, 0x75, 0x2d, 0xc9, 0x48, 0x2d,
-	0x4a, 0xca, 0xaf, 0x74, 0x2e, 0x4a, 0x4d, 0x2c, 0x49, 0x75, 0x4c, 0x4e, 0xce, 0x2f, 0xcd, 0x2b,
-	0x11, 0x12, 0xe2, 0x62, 0x49, 0x49, 0x2c, 0x49, 0x94, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x09, 0x02,
-	0xb3, 0x95, 0x94, 0xb9, 0x78, 0x61, 0x8a, 0x83, 0x4b, 0x12, 0x4b, 0x52, 0xb1, 0x2a, 0x5a, 0xc7,
-	0x88, 0x50, 0xe5, 0x58, 0x50, 0x10, 0x52, 0x21, 0x24, 0xc1, 0xc5, 0x5e, 0x96, 0x5a, 0x54, 0x9c,
-	0x99, 0x9f, 0x07, 0x56, 0xc8, 0x1a, 0x04, 0xe3, 0x82, 0xf4, 0x97, 0x54, 0x16, 0xa4, 0x4a, 0x30,
-	0x29, 0x30, 0x6a, 0x70, 0x06, 0x81, 0xd9, 0x42, 0xf6, 0x5c, 0x7c, 0xc9, 0x60, 0x97, 0xc4, 0x27,
-	0x42, 0x9c, 0x22, 0x21, 0xa0, 0xc0, 0xa8, 0xc1, 0x6d, 0x24, 0xa6, 0x87, 0xd5, 0xa1, 0x1e, 0x0c,
-	0x41, 0xbc, 0xc9, 0x28, 0x2e, 0x57, 0xe3, 0x62, 0x2d, 0x06, 0xb9, 0x4e, 0x82, 0x19, 0xac, 0x8f,
-	0x4f, 0x0f, 0xc5, 0xcd, 0x1e, 0x0c, 0x41, 0x10, 0x69, 0x27, 0x36, 0x88, 0xe3, 0x93, 0xd8, 0xc0,
-	0x21, 0x61, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xb4, 0x7c, 0xeb, 0x7a, 0x18, 0x01, 0x00, 0x00,
+	// 240 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0xc1, 0x4a, 0xc4, 0x30,
+	0x10, 0x86, 0x37, 0x6a, 0x57, 0x9d, 0xdd, 0xd5, 0x12, 0x04, 0x73, 0x2c, 0x05, 0xa1, 0x17, 0x7b,
+	0xd0, 0x17, 0xb0, 0x8a, 0xb0, 0xe7, 0xb8, 0x77, 0x49, 0xdb, 0x41, 0x05, 0x6d, 0x4a, 0x32, 0x6a,
+	0xf7, 0x0d, 0x7d, 0x2c, 0xc9, 0xc4, 0xc0, 0x22, 0xec, 0xed, 0xff, 0xc3, 0x37, 0xf0, 0x7f, 0x81,
+	0x05, 0x4d, 0x1f, 0xfe, 0xa5, 0x1e, 0x9d, 0x25, 0x5b, 0x5e, 0xc3, 0xe5, 0x23, 0xbd, 0xa2, 0x6b,
+	0xed, 0xf6, 0xc1, 0xa1, 0x21, 0x6c, 0xba, 0xce, 0x7e, 0x0e, 0xb4, 0x99, 0xa4, 0x84, 0xa3, 0xde,
+	0x90, 0x51, 0xa2, 0x10, 0xd5, 0x52, 0x73, 0x2e, 0xaf, 0xe0, 0x3c, 0xe1, 0x4f, 0x64, 0x08, 0xf7,
+	0x60, 0x3f, 0x02, 0x56, 0x89, 0x6b, 0xc6, 0x71, 0x33, 0x49, 0x05, 0xc7, 0x5f, 0xe8, 0xfc, 0x9b,
+	0x1d, 0x18, 0xcc, 0x74, 0xaa, 0xe1, 0x9e, 0xb6, 0x23, 0xaa, 0x83, 0x42, 0x54, 0xa7, 0x9a, 0xb3,
+	0xbc, 0x80, 0xcc, 0x7e, 0x0f, 0xe8, 0xd4, 0x21, 0x3f, 0xc6, 0x22, 0x1b, 0x38, 0xeb, 0x78, 0xe3,
+	0xb3, 0x89, 0x23, 0x55, 0x5e, 0x88, 0x6a, 0x71, 0xa3, 0xea, 0x3d, 0x0a, 0xeb, 0x99, 0x5e, 0x75,
+	0xbb, 0x4f, 0xb2, 0x82, 0xcc, 0x87, 0xdd, 0xea, 0x84, 0x2f, 0xf3, 0xfa, 0x9f, 0xcd, 0x7a, 0xa6,
+	0x23, 0x70, 0x3f, 0x8f, 0x5a, 0xe5, 0x1d, 0xe4, 0x3b, 0x26, 0x8c, 0x05, 0x19, 0xd3, 0xf7, 0x0e,
+	0xbd, 0xff, 0xb3, 0x4e, 0x35, 0xc8, 0xb4, 0xef, 0xb6, 0x65, 0x99, 0xa5, 0xe6, 0xdc, 0xce, 0xf9,
+	0xa7, 0x6f, 0x7f, 0x03, 0x00, 0x00, 0xff, 0xff, 0xc8, 0x47, 0x85, 0x8f, 0x78, 0x01, 0x00, 0x00,
 }
