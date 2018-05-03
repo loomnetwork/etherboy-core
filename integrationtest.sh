6234597c7421cb563519ec518ec6bfb451356099
Nishant Modak <modak.nishant@gmail.com>
2018-05-03 12:08:58 +0700

    add integration test script for in process

---
+++ integrationtest.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -ex
+
+echo "_______________________IN PROCESS PLUGIN__________________________________"
+
+echo "Using Loom Build ${loom_build}"
+
+echo "Using Etherboy Build ${etherboy_build}"
+
+mkdir -p /tmp/loom/contracts
+
+echo "Downloading loom sdk"
+
+gsutil cp gs://private.delegatecall.com/loom/linux/build-${loom_build}/loom /tmp/loom/loom-linux
+
+echo "Downloading etherboy plugin"
+
+echo "Downloading etherboy plugin"
+
+gsutil cp gs://private.delegatecall.com/etherboy/linux/build-${etherboy_build}/etherboycore.so /tmp/loom/contracts/etherboycore.so
+
+echo "Downloading etherboy cli"
+
+gsutil cp gs://private.delegatecall.com/etherboy/linux/build-${etherboy_build}/etherboycli /tmp/loom/etherboycli
+
+cd /tmp/loom
+
+chmod +x loom-linux
+chmod +x etherboycli
+
+./loom-linux init --force
+
+rm genesis.json
+
+echo "{
+    \"contracts\": [
+        {
+            \"vm\": \"plugin\",
+            \"format\": \"plugin\",
+            \"location\": \"etherboycore:0.0.1\",
+            \"init\": {
+
+            }
+        }
+    ]
+}
+" >> genesis.json
+
+
+./loom-linux run > loom_run_${etherboy_build}_${loom_build}.log 2>&1 &
+
+sleep 5
+
+./etherboycli genkey -k key
+
+./etherboycli create-acct -k key
+
+pkill -f loom-linux
+
+cat loom_run_${etherboy_build}_${loom_build}.log
+
+
+echo "_______________________OUT PROCESS PLUGIN__________________________________"
--- jenkins.sh
+++ jenkins.sh
@@ -2,26 +2,41 @@
 
 set -ex
 
-export GOPATH=~/gopath
+export GOPATH=/tmp/gopath-jenkins-loom-sdk-pipeline-test-${loom_sdk_pipeline_build_number}
 ln -sfn `pwd` $GOPATH/src/github.com/loomnetwork/etherboy-core
 cd $GOPATH/src/github.com/loomnetwork
+
+# Building Go Loom
+
+echo "Building Go Loom"
 
 if cd go-loom; then
   ## Always build with latest go-loom
   git pull origin master
 else
-  git clone git@github.com:loomnetwork/go-loom.git $GOPATH/src/github.com/loomnetwork/go-loom 
+  git clone git@github.com:loomnetwork/go-loom.git $GOPATH/src/github.com/loomnetwork/go-loom
   cd go-loom
 fi
 
 ## Building go-loom
 make clean
 make deps
-make 
+make
+make test
 cd ..
+
+echo "Building Etherboy"
 
 ## Building etherboy-core
 cd etherboy-core
+go get github.com/pkg/errors
 make clean
 make deps
-make 
+make
+
+
+echo ${BUILD_NUMBER}
+echo ${JOB_NAME}
+
+gsutil cp run/contracts/etherboycore.so gs://private.delegatecall.com/etherboy/linux/build-$BUILD_NUMBER/etherboycore.so
+gsutil cp run/cmds/etherboycli gs://private.delegatecall.com/etherboy/linux/build-$BUILD_NUMBER/etherboycli
